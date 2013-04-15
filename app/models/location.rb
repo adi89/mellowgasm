16bebad4df17f089dabbe088beae6f2acdb3de18
Adi Singh <adityasingh89@gmail.com>
2013-04-14 22:34:35 -0400

    ready to pull and i added some more comments to home controller as well as an actual method

--- app/models/location.rb
+++ app/models/location.rb
@@ -14,9 +14,16 @@ class Location < ActiveRecord::Base
 attr_accessible :address, :latitude, :longitude
 belongs_to :user
 has_many :venues
+before_save :latlng
 
 
-
-
+def latlng
+  result = Geocoder.search(self.address).first
+  if result.present?
+      self.latitude = result.latitude
+      self.longitude = result.longitude
+    end
+  end
+#we save the searches in the database.
 
 end
