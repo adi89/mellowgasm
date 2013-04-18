f2d46442fa247b0db3e6034ed57fc8361ff4a56a
Adi Singh <adityasingh89@gmail.com>
2013-04-18 10:36:45 -0400

    refactor models a bit

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -28,9 +28,6 @@ class Venue < ActiveRecord::Base
   attr_accessible :name, :address, :rating, :total_votes, :latitude, :longitude, :crossStreet, :foursquare_identification, :phone, :distance, :twitter
   has_many :photos
   belongs_to :location
-  # validates :foursquare_identification, :uniqueness => true
-  # after_save :ratio
-  # after_save :rate
 
   def self.ratio(venue)
     client = Foursquare2::Client.new(:client_id => ENV["F4_CLIENT"], :client_secret => ENV["F4_CLIENT_SECRET"])
@@ -64,9 +61,6 @@ class Venue < ActiveRecord::Base
       a.save
       location.venues << a
       motivation.venues << a
-      # Venue.ratio(a)
-      # location.venues << venue
-      # motivation.venues << venue
      end
   end
 
@@ -74,21 +68,10 @@ def self.algorithm(ratio)
   y = -4 * (ratio - 0.6)**2 + 1
 end
 
-
   def self.top_picks
 
     Venue.all.sort_by{|i| (0.35* Venue.algorithm(i.ratio) + 0.4 * i.foursquare_rating +  0.25 * i.checkins)}.last(10).map{|i| i.name}.reverse.
   end
 
-  def rate
-    ratio = (self.ratio/0.70)
-    y =  -4 * (ratio - 0.6)**2 + 1
-    client = Foursquare2::Client.new(:client_id => ENV["F4_CLIENT"], :client_secret => ENV["F4_CLIENT_SECRET"])
-    a = client.venue(self.foursquare_identification)
-    checkins = (a["stats"]["checkinsCount"].to_f)/30000.00
-    foursqRating = (a["rating"].to_f/10.00)
-    self.rating = ((foursqRating * 0.40) + (checkins * 0.25) + (y * 0.35)).round(2)
-    self.save
-  end
 
 end
